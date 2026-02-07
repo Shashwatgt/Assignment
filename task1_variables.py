65f2831a08d2c71e7f025568c75a2412dd5b56db
unknown <itsshashwat02@gmail.com>
2026-02-07 19:43:47 +0545

    Variable Modeling & Data Representation

---
+++ task1_variables.py
@@ -0,0 +1,15 @@
+full_name = "Shashwat Gautam"
+age = 24
+cgpa = 8.5
+current_semester = 8
+enrollment_status = True
+
+print(full_name, type(full_name))
+print(age, type(age))
+print(cgpa, type(cgpa))
+print(current_semester, type(current_semester))
+print(enrollment_status, type(enrollment_status))
+
+print("After changing value of age")
+age = "twenty-four"
+print(age, type(age))
